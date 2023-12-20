c3663a64aa10c61cc07a979b7e0a4f44e752d8dd
Anh Chu <34973633+anh-chu@users.noreply.github.com>
2023-12-20 14:37:15 +0700

    Update replace_env_vars.sh

--- frontend/replace_env_vars.sh
+++ frontend/replace_env_vars.sh
@@ -2,5 +2,5 @@
 ROOT_DIR=./
 
 # Replace env vars in JavaScript and HTML files served by NGINX
-find "$ROOT_DIR" -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i 's|VITE_NAME_PLACEHOLDER|'"$VITE_NAME"'|g' {} +
-find "$ROOT_DIR" -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i 's|VITE_API_URL_PLACEHOLDER|'"$VITE_API_URL"'|g' {} +
+find "$ROOT_DIR" -path ./proc/tty/driver -prune -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i 's|VITE_NAME_PLACEHOLDER|'"$VITE_NAME"'|g' {} +
+find "$ROOT_DIR" -path ./proc/tty/driver -prune -type f \( -name "*.js" -o -name "*.html" \) -exec sed -i 's|VITE_API_URL_PLACEHOLDER|'"$VITE_API_URL"'|g' {} +
